0c78714c72e392176e8a62474ca2ad16374b0e9b
Aggressivejonty009 <108817943+Aggressivejonty009@users.noreply.github.com>
2022-12-17 11:32:10 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,1 +1,23 @@
-import RNDM()
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from RNDM import login
+
+        login()
