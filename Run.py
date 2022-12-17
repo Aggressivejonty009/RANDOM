f97e1ea50c5ac6da3f6037ca87dde3d2ba732bfb
Aggressivejonty009 <108817943+Aggressivejonty009@users.noreply.github.com>
2022-12-17 11:42:23 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,23 +1,23 @@
-import os, platform
+import RNDM
 
-try:
 
-    import requests
 
-except:
 
-    os.system('pip install requests')
 
-os.system('git pull')
 
-import requests
 
-os.system('git pull')
 
-bit = platform.architecture()[0]
 
-if bit == "64bit":
 
-        from RNDM import manu
 
-        __main__()
+
+
+
+
+
+
+
+
+
+
+
